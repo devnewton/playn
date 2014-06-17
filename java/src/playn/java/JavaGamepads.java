3f9f8bc9e54afa3bb9ae081e2eb267c7d4a309e1
bob <bob@192.168.1.71>
2014-06-17 20:21:10 +0200

    add missing license headers

--- java/src/playn/java/JavaGamepads.java
+++ java/src/playn/java/JavaGamepads.java
@@ -1,9 +1,23 @@
+/**
+ * Copyright 2014 The PlayN Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package playn.java;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Controllers;
 import playn.core.Gamepad;
@@ -12,7 +26,7 @@ import playn.core.Platform;
 
 /**
  *
- * @author devnewton
+ * @author devnewton <devnewton@bci.im>
  */
 class JavaGamepads implements Gamepads {
 
